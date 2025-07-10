67364acc08b935f161c0200ba1e891f9a7acf030
supremeonetech <okaforfriday14@gmail.com>
2025-07-10 23:48:38 +0100

    JS

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,5 +1,3 @@
-<script>
-    const input = document.querySelector('#favchap'); const button =
-    document.querySelector('buttonb'); const list =
-    document.querySelector('___________');
-</script>;
+const input = document.querySelector('#favchap'); const button =
+document.querySelector('buttonb'); const list =
+document.querySelector('___________');
